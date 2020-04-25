5b0e3e755f68c267f932e626d8dc0b19b9594e0b
RyanB <rbutler81@gmail.com>
2020-04-25 15:06:43 -0400

    added method

---
+++ Logger/src/ConfigException.java
@@ -0,0 +1,13 @@
+public class ConfigException extends Exception {
+
+	private static final long serialVersionUID = 5427096097823764186L;
+
+	public ConfigException(String message){
+		super(message);
+	}
+	
+	public String getMessage() {
+		return super.getMessage();
+	}
+
+}
---
+++ Logger/src/LogConfig.java
@@ -0,0 +1,112 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogConfig {
+
+	private int port;
+	private int maxLogSizeBytes;
+	private int oldLogsToKeep;
+	
+	
+	public LogConfig(int port, int maxLogSizeBytes, int oldLogsToKeep) throws ConfigException {
+		
+		try {
+			this.setPort(port);
+			this.setMaxLogSizeBytes(maxLogSizeBytes);
+			this.setOldLogsToKeep(oldLogsToKeep);
+		}
+		catch (ConfigException e) {
+			throw e;
+		}
+		
+	}
+	
+	public int getPort() {
+		return port;
+	}
+	public LogConfig setPort(int port) throws ConfigException {
+		
+		if (port > 0 && port <= 65535) {
+			this.port = port;
+			return this;
+		}
+		else {
+			throw new ConfigException("Port must be between 0 - 65535");
+		}
+		
+	}
+	public int getMaxLogSizeBytes() {
+		return maxLogSizeBytes;
+	}
+	public LogConfig setMaxLogSizeBytes(int maxLogSizeBytes) throws ConfigException {
+		
+		if (maxLogSizeBytes > 0) {
+			this.maxLogSizeBytes = maxLogSizeBytes;
+			return this;
+		}
+		else {
+			throw new ConfigException("maxLogSizeBytes must be greater than 0");
+		}
+	}
+	public int getOldLogsToKeep() {
+		return oldLogsToKeep;
+	}
+	public LogConfig setOldLogsToKeep(int oldLogsToKeep) throws ConfigException {
+		
+		if (oldLogsToKeep >= 1) {
+			this.oldLogsToKeep = oldLogsToKeep;
+			return this;
+		}
+		else {
+			throw new ConfigException("oldLogsToKeep must be greater to, or equal to 1");
+		}
+	}
+	
+	static LogConfig fromFile(String str) throws ConfigException{
+		
+		int port = 0;
+		int maxBytes = 0;
+		int maxOld = 0;		
+		
+		try {
+			
+			BufferedReader br = new BufferedReader(new FileReader(str));
+			String line = br.readLine();
+			List<String> p = new ArrayList<String>();
+			p.add(line);
+			
+			
+		
+			while (line != null) {
+				line = br.readLine();
+				p.add(line);
+			}
+			
+			for (String s : p) {
+				
+				if (s != null) {
+				
+					if (s.startsWith("#port=")) {
+							port = Integer.parseInt(s.substring(6));
+						}
+						else if (s.startsWith("#maxLogSizeBytes=")) {
+							maxBytes = Integer.parseInt(s.substring(17));
+						}
+						else if (s.startsWith("#oldLogsToKeep=")) {
+							maxOld = Integer.parseInt(s.substring(15));
+						}
+					
+					}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return new LogConfig(port, maxBytes, maxOld);
+		
+	}
+	
+}
