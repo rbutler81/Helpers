616831c64641caaa8794bde2c4a4de385bf5b853
RyanB <rbutler81@gmail.com>
2020-04-29 00:43:00 -0400

    added package

---
+++ Comms/src/udp/SendUdp.java
@@ -0,0 +1,46 @@
+package udp;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class SendUdp {
+
+    protected <T extends Serializable> boolean sendObjectTo(String addr, int port, T o) {
+
+        boolean r = false;
+
+        try {
+            // setup address to transmit to and socket
+            InetAddress address = InetAddress.getByName(addr);
+            DatagramSocket clientSocket = new DatagramSocket();
+
+            // prepare data
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.flush();
+            oos.writeObject(o);
+            oos.flush();
+            byte[] dataToSend = baos.toByteArray();
+            oos.close();
+            baos.close();
+            // create datagram packet
+            DatagramPacket packet = new DatagramPacket(dataToSend, dataToSend.length, address, port);
+
+            // send data
+            clientSocket.send(packet);
+
+            r = true;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return r;
+
+    }
+
+}
